ee9b66d56b4e777ef021496e3b9cfa4663910824
fernandezdlg <juan.fernandezdelagarza@unibe.ch>
2025-02-12 16:01:52 +0200

    refactor

--- compile.sh
+++ compile.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # This script performs the compilation of aff via its Makefile
 
-# Load the required modules
-source ../../load_modules.sh
-
 make clean
 
 # Config the build
 make CONFIG=linux
 
 # Install in the build directory
-timestamp=$(date +"%Y-%m-%d_%H:%M")  # Format: YYYY-MM-DD_HH:MM
-make CONFIG=linux install prefix="./build-${timestamp}"
+# timestamp=$(date +"%Y-%m-%d_%H:%M")  # Format: YYYY-MM-DD_HH:MM
+# make CONFIG=linux install prefix="./build-${timestamp}"
+
+make CONFIG=linux install prefix="./build"
